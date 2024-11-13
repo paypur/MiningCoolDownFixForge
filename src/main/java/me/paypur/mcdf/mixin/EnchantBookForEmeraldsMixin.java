cef8b905fa092f9c13ed490dbea80a82b6efe523
paypur <pieceofpaypur@gmail.com>
2024-11-13 14:02:22 -0500

    Refactor mixin
    Took 60 minutes

--- src/main/java/me/paypur/mcdf/mixin/EnchantBookForEmeraldsMixin.java
+++ src/main/java/me/paypur/mcdf/mixin/EnchantBookForEmeraldsMixin.java
@@ -1,54 +1,59 @@
 package me.paypur.mcdf.mixin;
 
 import me.paypur.mcdf.MCDF;
-import net.minecraft.core.Registry;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.EnchantedBookItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentInstance;
-import net.minecraft.world.item.trading.MerchantOffer;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
 import net.minecraft.world.entity.npc.VillagerTrades;
-import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(VillagerTrades.EnchantBookForEmeralds.class)
 public class EnchantBookForEmeraldsMixin {
 
-    @Final
-    @Shadow
-    private int villagerXp;
+    @Redirect(
+            method = "getOffer(Lnet/minecraft/world/entity/Entity;Ljava/util/Random;)Lnet/minecraft/world/item/trading/MerchantOffer;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/enchantment/Enchantment;getMinLevel()I"
+            )
+    )
+    public int getMinLevel(Enchantment pEnchantment) {
+        return pEnchantment.equals(MCDF.COOLDOWN.get()) ? 0 : pEnchantment.getMinLevel();
+    }
 
-    /**
-     * @author paypur
-     * @reason makes cooldown much cheaper
-     */
-    @Overwrite
-    public MerchantOffer getOffer(Entity pTrader, Random pRand) {
-        List<Enchantment> list = Registry.ENCHANTMENT.stream().filter(Enchantment::isTradeable).collect(Collectors.toList());
-        Enchantment enchantment = list.get(pRand.nextInt(list.size()));
-        boolean f = !enchantment.equals(MCDF.COOLDOWN.get());
-        int i = f ? Mth.nextInt(pRand, enchantment.getMinLevel(), enchantment.getMaxLevel()) : 1;
-        ItemStack itemstack = EnchantedBookItem.createForEnchantment(new EnchantmentInstance(enchantment, i));
-        int j = f ? 2 + pRand.nextInt(5 + i * 10) + 3 * i : 1;
-        if (enchantment.isTreasureOnly()) {
-            j *= 2;
-        }
+    @Redirect(
+            method = "getOffer(Lnet/minecraft/world/entity/Entity;Ljava/util/Random;)Lnet/minecraft/world/item/trading/MerchantOffer;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/enchantment/Enchantment;getMaxLevel()I"
+            )
+    )
+    public int getMaxLevel(Enchantment pEnchantment) {
+        return pEnchantment.equals(MCDF.COOLDOWN.get()) ? 0 : pEnchantment.getMaxLevel();
+    }
 
-        if (j > 64) {
-            j = 64;
-        }
+    @ModifyArg(
+            method = "getOffer(Lnet/minecraft/world/entity/Entity;Ljava/util/Random;)Lnet/minecraft/world/item/trading/MerchantOffer;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/enchantment/EnchantmentInstance;<init>(Lnet/minecraft/world/item/enchantment/Enchantment;I)V"
+            ),
+            index = 1
+    )
+    private int EnchantmentInstance(Enchantment pEnchantment, int pLevel){
+        return pEnchantment.equals(MCDF.COOLDOWN.get()) ? 1 : pLevel;
+    }
 
-        return new MerchantOffer(new ItemStack(Items.EMERALD, j), new ItemStack(Items.BOOK), itemstack, 12, this.villagerXp, 0.2F);
+    @Redirect(method = "getOffer(Lnet/minecraft/world/entity/Entity;Ljava/util/Random;)Lnet/minecraft/world/item/trading/MerchantOffer;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/enchantment/Enchantment;isTreasureOnly()Z"
+            )
+    )
+    private boolean isTreasureOnly(Enchantment instance) {
+        return !instance.equals(MCDF.COOLDOWN.get()) && instance.isTreasureOnly();
     }
 
 }
